c27b97ae9c9647bba4393c370563ab6b20f4f71a
Zoltan Gobolos <zgobolos@yaffaworks.com>
2020-10-22 13:06:59 +0200

    fix determining app version

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const path = require('path');
 global.Promise = require('bluebird');
 
 // eslint-disable-next-line import/no-dynamic-require
-const packageJson = require(path.resolve('package.json'));
+const packageJson = require(path.join(__dirname, 'package.json'));
 
 program.version(packageJson.version);
 program
